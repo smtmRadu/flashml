a0106931b8c8238c071bc978fafded26540e8145
Radu  Ciobanu <radu.ciobanu@gshmedia.com>
2025-10-20 09:01:18 +0300

    v0.6.5.25 little patch

--- flashml/llm/__init__.py
+++ flashml/llm/__init__.py
@@ -5,19 +5,21 @@ from .vllm_chat import vllm_chat
 from .vllm_compute_log_probs import vllm_compute_logprobs
 from .vllm_engine import vllm_close
 from .llm_utils import get_4bit_quantization_config, get_boxed_answer, merge_and_quantize_llm
-from .vllm_chat_openai_entrypoint import (
-    vllm_chat_openai_entrypoint, 
-    QWEN3_06B_DEFAULT_ARGS, 
+from .vllm_chat_openai_entrypoint import vllm_chat_openai_entrypoint
+    
+
+from .vllm_configs import (QWEN3_06B_DEFAULT_ARGS, 
     QWEN3_4B_INSTRUCT_2507_DEFAULT_ARGS,
     GPT_OSS_120B_HIGH_DEFAULT_ARGS, 
     GPT_OSS_20B_LOW_DEFAULT_ARGS, 
-    MAGISTRAL_SMALL_2506_DEFAULT_ARGS)
-
+    MAGISTRAL_SMALL_2506_DEFAULT_ARGS,
+    MISTRAL_SMALL_2506_DEFAULT_ARGS)
 
 __all__ = [
     "GPT_OSS_120B_HIGH_DEFAULT_ARGS",
     "GPT_OSS_20B_LOW_DEFAULT_ARGS",
     "MAGISTRAL_SMALL_2506_DEFAULT_ARGS",
+    "MISTRAL_SMALL_2506_DEFAULT_ARGS",
     "QWEN3_06B_DEFAULT_ARGS",
     "QWEN3_4B_INSTRUCT_2507_DEFAULT_ARGS",
     
